613eec6fd5f10986a3ccb6e895c237e1ef548cc5
Dan Gunter <dkgunter@lbl.gov>
2014-05-30 21:33:35 -0700

    runs

--- fireworks/benchmarks/bench.py
+++ fireworks/benchmarks/bench.py
@@ -13,6 +13,7 @@ __date__ = '5/30/14'
 import csv
 import logging
 import os
+import re
 import sys
 import time
 # Third-party
@@ -21,10 +22,28 @@ import pymongo
 from fireworks import FireWork, FWorker, LaunchPad, fw_config
 from fireworks import FireTaskBase, FWAction, Workflow
 from fireworks.core.rocket_launcher import rapidfire
+from fireworks.features.multi_launcher import launch_multiprocess
+
+# Set up paths
+path = 'fireworks.benchmarks'
+if not path in fw_config.USER_PACKAGES:
+    fw_config.USER_PACKAGES.append(path)
 
 
-def print_timings(tm, meta, header=False):
-    w = csv.DictWriter(sys.stdout, ["name", "tm_sec"] + meta.keys())
+def print_timings(tm, meta, header=False, strm=None):
+    """Print timings in 'tm', along with metadata in 'meta', as CSV.
+
+    Both 'tm' and 'meta' are dictionaries. One row will be written for
+    each key in 'tm', with the name set to the key and the timing set to
+    the (numeric) value. The key/value pairs from 'meta' will
+    be included as additional columns whose values are repeated for each row.
+
+    If 'header' is True, then the CSV header will be printed first.
+
+    Output will go to 'strm' if given, otherwise stdout.
+    """
+    f = strm or sys.stdout
+    w = csv.DictWriter(f, ["name", "tm_sec"] + meta.keys())
     if header:
         w.writeheader()
     d = meta.copy()
@@ -33,90 +52,144 @@ def print_timings(tm, meta, header=False):
         w.writerow(d)
 
 
-class EmptyTask(FireTaskBase):
-    _fw_name = "Empty Task"
+class BenchmarkTask(FireTaskBase):
+    _fw_name = "Benchmark Task"
 
     def run_task(self, fw_spec):
         return FWAction(stored_data={})
 
 
 class Benchmark(object):
-    task_class = EmptyTask
+    task_class = BenchmarkTask
+    database = "fireworks_bench"
 
-    def __init__(self):
+    # Set up logging
+    log = logging.getLogger("benchmark")
+    _hnd = logging.StreamHandler()
+    _hnd.setFormatter(
+        logging.Formatter("(%(name)s) %(levelname)s - %(message)s"))
+    log.addHandler(_hnd)
 
-        # Set up logging
-        _log = logging.getLogger("benchmark")
-        _hnd = logging.StreamHandler()
-        _hnd.setFormatter(
-            logging.Formatter("(%(name)s) %(levelname)s - %(message)s"))
-        _log.addHandler(_hnd)
-        if 'FW_BENCH_QUIET' in os.environ:
+    def __init__(self, vb=0):
+        # remember initial dir (FireWorks does chdir)
+        self._pdir = os.getcwd()
+        if vb < 0 or 'FW_BENCH_QUIET' in os.environ:
             lvl = logging.CRITICAL
-        elif 'FW_BENCH_DEBUG' in os.environ:
+        elif vb > 1 or 'FW_BENCH_DEBUG' in os.environ:
             lvl = logging.DEBUG
-        elif 'FW_BENCH_INFO' in os.environ:
+        elif vb > 0 or 'FW_BENCH_INFO' in os.environ:
             lvl = logging.INFO
         else:
             lvl = logging.WARN
-        _log.setLevel(lvl)
-        self.loglevel, self.log = logging.getLevelName(lvl), _log
-
-        # Set up paths
-        fw_config.USER_PACKAGES.append('fireworks.benchmarks')
+        self.log.setLevel(lvl)
+        self.loglevel = logging.getLevelName(lvl)
 
         try:
-            self.lpad = LaunchPad(name='fireworks_test', strm_lvl='ERROR')
+            self.lpad = LaunchPad(name=self.database, strm_lvl=self.loglevel)
         except pymongo.errors.ConnectionFailure:
             self.log.error("Failed to connect to MongoDB: is it running?")
             raise
+
+    def reset(self):
         self.lpad.reset('', require_password=False)
 
-    def run_local(self, wf):
-        self.log.info("run_local.start")
-        add_t = time.time()
-        self.lpad.add_wf(wf)
-        add_t = time.time() - add_t
-        run_t = time.time()
-        rapidfire(self.lpad, FWorker(), strm_lvl=self.loglevel)
-        run_t = time.time() - run_t
-        self.log.info("run_local.end")
-        return dict(add=add_t, run=run_t)
+    def _reset_dir(self):
+        os.chdir(self._pdir)
 
-    def get_workflow(self, n, task_class=None):
+    def load(self, wf):
+        self.log.info("bench.load.start")
+        load_t = time.time()
+        self.lpad.add_wf(wf)
+        load_t = time.time() - load_t
+        self.log.info("bench.load.end")
+        return load_t
+
+    def run(self, np):
+        self.log.info("bench.run.start np={:d}".format(np))
+        run_t = time.time()
+        if np <= 1:
+            rapidfire(self.lpad, FWorker(), strm_lvl=self.loglevel)
+        else:
+            launch_multiprocess(self.lpad, FWorker(), self.loglevel, 0,
+                                np, 0.1, total_node_list=None, ppn=1)
+        run_t = time.time() - run_t
+        self.log.info("bench.run.end np={:d}".format(np))
+        return run_t
+
+    def _rmdir(self, d, pat):
+        """Recursively remove all files/dirs matching 'pat' in path 'd'.
+        """
+        pat_re = re.compile(pat)
+        for p in os.listdir(d):
+            if not pat_re.match(p):
+                continue
+            path = os.path  .join(d, p)
+            if os.path.isdir(path):
+                self._rmdir(path, ".*")
+                os.rmdir(path)
+            else:
+                os.unlink(path)
+
+    def cleanup(self):
+        self.log.info("bench.cleanup.start")
+        self._rmdir(self._pdir, "launcher_.*")
+        self.log.info("bench.cleanup.end")
+
+    def get_workflow(self, n, task_class=None, deps="sequence"):
         """Create a workflow with 'n' tasks.
 
-        Dependencies are structured so that the second task
-        depends on the first, and every task after the second
-        depends on the two previous ones.
-
         :param n: Number of tasks
-        :type n: int
+        :type n: int, >= 1
+        :param task_class: Task class or use self.task_class
+        :param deps: Type of dependencies
+            "sequence" -  No dependencies between tasks.
+            "reduce"   -  The last task depends on all previous ones.
+            "complex"  -  Dependencies are structured so that the second task
+                          depends on the first, and every task after the second
+                          depends on the two previous ones.
+        :type deps: str
         """
+        if n < 1:
+            raise ValueError("Bad #tasks: {}".format(n))
         if task_class is None:
             task_class = self.task_class
         tasks = [task_class() for _ in xrange(n)]
         fireworks = [FireWork(tasks[i], fw_id=i) for i in xrange(n)]
-        if n == 1:
-            deps = {}
-        elif n == 2:
-            deps = {0: [1]}
+        if deps == "sequence":
+            _deps = {}
+        elif deps == "reduce":
+            if n == 1:
+                _deps = {}
+            else:
+                _deps = {n - 1: range(n - 1)}
+        elif deps == "complex":
+            if n == 1:
+                _deps = {}
+            elif n == 2:
+                _deps = {0: [1]}
+            else:
+                _deps = {}
+                for i in xrange(n - 2):
+                    _deps[i] = [i+1, i+2]
+                _deps[n - 2] = [n - 1]
         else:
-            deps = {}
-            for i in xrange(n - 2):
-                deps[i] = [i+1, i+2]
-            deps[n - 2] = [n - 1]
-        workflow = Workflow(fireworks, deps)
+            raise ValueError("Bad value for 'deps': {}".format(deps))
+        workflow = Workflow(fireworks, _deps)
         return workflow
 
 
 def example():
     bench = Benchmark()
-    first = True
+    # warmup
+    bench.load(bench.get_workflow(1))
+    print_timings({}, {'n': 0}, header=True)
+    # main
+    tm = {}
     for n in (1, 2, 3):
-        tm = bench.run_local(bench.get_workflow(n))
-        print_timings(tm, {'n': n}, header=first)
-        first = False
+        tm['load'] = bench.load(bench.get_workflow(n))
+        tm['run'] = bench.run()
+        print_timings(tm, {'n': n})
+    bench.cleanup()
 
 if __name__ == '__main__':
     example()
---
+++ fireworks/benchmarks/bench_scaling.py
@@ -0,0 +1,103 @@
+"""
+Simple scaling study.
+"""
+__author__ = 'Dan Gunter <dkgunter@lbl.gov>'
+__date__ = '5/30/14'
+
+# System
+import argparse
+import re
+import sys
+import threading
+# Local
+from fireworks.benchmarks.bench import Benchmark, print_timings
+
+
+def warmup(bench, first):
+    # warmup
+    bench.log.info("warmup.start")
+    bench.load(bench.get_workflow(1, deps="sequence"))
+    if first:
+        print_timings({}, {'nwf': 0, 'ntasks': 0, 'type': ''},
+                      header=True)
+    bench.log.info("warmup.end")
+
+
+def load(bench, tasks, workflows, deps):
+    for ntasks in range(*tasks):
+        for nwf in range(*workflows):
+            warmup(bench, False)
+            # run 'nwf' workflows with 'ntasks' tasks in each
+            bench.log.info("load.start nwf={:d} ntasks={:d}"
+                           .format(nwf, ntasks))
+            tm = 0
+            for w in xrange(nwf):
+                tm += bench.load(bench.get_workflow(ntasks))
+            bench.log.info("load.end")
+            print_timings({'load': tm},
+                          {'nwf': nwf, 'ntasks': ntasks, 'type': deps})
+
+
+def run(bench, meta, np):
+    bench.log.info("scaling.run.start")
+    tm = bench.run(np)
+    bench.log.info("scaling.run.end")
+    print_timings({'run': tm}, meta)
+
+
+def irange(v):
+    m = re.match("(\d+)(?::(\d+):(\d+))?", v)
+    if m is None:
+        raise ValueError("Bad range min:max:step in '{}'".format(v))
+    g = m.groups()
+    if g[1] is None:
+        return int(g[0]), int(g[0]) + 1, 1
+    else:
+        return int(g[0]), int(g[1]) + 1, int(g[2])
+
+
+def main():
+    ap = argparse.ArgumentParser("Load or run workflows")
+    ap.add_argument("--mode", dest="mode", default="load",
+                    help="Mode: load, run")
+    ap.add_argument("--tasks", dest="tasks", type=irange, default='1',
+                    help="Number of tasks")
+    ap.add_argument("--type", dest="type", default="sequence",
+                    help="Workflow type (sequence, reduce, complex)")
+    ap.add_argument("--workflows", dest="workflows", type=irange, default='1',
+                    help="Number of workflows")
+    ap.add_argument("--reset", dest="reset", action="store_true",
+                    help="Reset the FireWorks DB before starting")
+    ap.add_argument("--np", dest="np", type=int, default=1,
+                    help="Parallelism, for 'run' mode only")
+    ap.add_argument("-v", "--verbose", dest="vb", action="count", default=0,
+                    help="Increase log level to INFO, then DEBUG")
+    ap.add_argument("-q", "--quiet", dest="quiet", action="store_true",
+                    help="Turn off log messages")
+    args = ap.parse_args()
+    vb = -1 if args.quiet else min(args.vb, 2)
+
+    bench = Benchmark(vb=vb)
+    if args.reset:
+        bench.reset()
+    warmup(bench, True)
+
+    if args.mode == "load":
+        load(bench, args.tasks, args.workflows, args.type)
+    elif args.mode == "run":
+        max_wf, max_tasks = args.workflows[1] - 1, args.tasks[1] - 1
+        meta = {'nwf': max_wf, 'ntasks': max_tasks, 'type': args.type}
+        run(bench, meta, args.np)
+    else:
+        ap.error("Bad mode '{}' not load or run".format(args.mode))
+
+    bench.log.info("scaling.cleanup.start")
+    bench.cleanup()
+    bench.log.info("scaling.cleanup.end")
+
+    return 0
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
